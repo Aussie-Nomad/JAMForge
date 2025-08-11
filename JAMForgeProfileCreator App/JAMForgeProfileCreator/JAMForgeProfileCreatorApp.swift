9372680221b548d009b15b7bf57fcb39c0202fce
Daniel McDermott <xenomophicdesigns@gmail.com>
2025-08-11 12:04:22 +0100

    Combining initial commit with Xcode project setup

---
+++ JAMForgeProfileCreator App/JAMForgeProfileCreator/JAMForgeProfileCreatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  JAMForgeProfileCreatorApp.swift
+//  JAMForgeProfileCreator
+//
+//  Created by Danny Mac on 11/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct JAMForgeProfileCreatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
